de6ecfa5f125c62d8e09cfa179a445c239c54672
timklabjan <tklabz28@gmail.com>
2017-10-22 16:30:11 -0500

    Update Breakthrough.py

--- Breakthrough.py
+++ Breakthrough.py
@@ -58,6 +58,18 @@ def makeMove(idx,finidx,board):
     board[finidx[0]][finidx[1]] = curr
     return board
 
+def off_heur(player,board):
+    if player == 'W':
+        return 2.0*(30-len(getPositions(board)[1])) + random()
+    if player == 'B':
+        return 2.0*(30-len(getPositions(board)[0])) + random()
+    
+def def_heur(player,board):
+    if player == 'W':
+        return 2.0*len(getPositions(board)[0]) + random()
+    if player == 'B':
+        return 2.0*len(getPositions(board)[1]) + random()
+
 board = createInitialBoard()
 b2 = makeMove((0,0), (4, 4), board)
 
