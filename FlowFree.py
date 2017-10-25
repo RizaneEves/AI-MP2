531df8d7c4b01df3d89207f96a296c0aa1c24538
timklabjan <tklabz28@gmail.com>
2017-10-25 16:21:03 -0500

    most constrained variable

--- FlowFree.py
+++ FlowFree.py
@@ -21,12 +21,32 @@ def recursiveBacktracking(assignment, domain, sources):
 
     return None
 
+def recursiveBacktracking_mcv(assignment, domain, sources):
+    indexes = Utilities.getAllUnassignedIndexNearColored(assignment)
+    index = Utilities.getMostConstrainedVariable(indexes,domain,assignment,sources)
+    #index = Utilities.getUnassignedIndexNearColored(assignment)
+    #print(index)
+    if(index is None):
+        if(Utilities.isAssignmentValid(assignment, sources)):
+            return assignment
+        return None
 
+    for value in domain:
+        if Utilities.isValueConsistent(index, value, assignment, sources):
+            #print("Assigning " + value + " to " + "(" + str(index[0]) + ", " + str(index[1]) + ")\n")
+            assignment[index[0]][index[1]] = value
+            #print(np.array(assignment))
+            result = recursiveBacktracking(assignment, domain, sources)
+            if result is not None:
+                return result
+            assignment[index[0]][index[1]] = "_"
 
-array = Utilities.parseArray("input77.txt")
+    return None
+
+array = Utilities.parseArray("sample.txt")
 domain = Utilities.getDomain(array)
 sources = Utilities.getSources(array)
 
-result = recursiveBacktracking(array, domain, sources)
-print(result)
-Utilities.writeArrayToFile(result, "output77.txt")
+result = recursiveBacktracking_mcv(array, domain, sources)
+print(np.array(result))
+Utilities.writeArrayToFile(result, "sample_sol.txt")
